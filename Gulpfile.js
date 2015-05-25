c203dc810830066a06e1f482e85d54e0534536b9
Sunny Lam <sunny.lam@gmail.com>
2015-05-25 17:57:57 -0400

    updated the Gulpfile and package.json for Autoprefixr and plumber (so that errors don't end gulp)

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 
 gulp.task('styles', function() {
   gulp.src('sass/*.scss')
+  	.pipe(plumber())
     .pipe(sass({
       errLogToConsole: true
     }))
     .pipe(concat('style.css'))
+	.pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('./'))
 });
 
