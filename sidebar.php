67f53dd08912a941e1255bb997904e614b8382ad
Sunny Lam <sunny.lam@gmail.com>
2015-05-27 15:55:14 -0400

    Reversed page.php changes and removed all test styling.

--- sidebar.php
+++ sidebar.php
@@ -13,21 +13,4 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 
-	<!-- ====================================
-	Room Attributes 
-	==================================== -->
-
-	<h2>Room Attributes</h2>
-
-	<ul class="roomAttributes">
-		<li>Free WiFi Connection</li>
-		<li>Free Entry to the Rooftop Spa &amp; Health Club</li>
-		<li>Mineral water in the room upon arrival</li>
-		<li>24-hour room service</li>
-		<li>Multimedia TV, Minibar, In-room Safe, Individually Controlled A/C</li>
-		<li>24-Hour In Room Dining Service</li>
-	</ul>
-
-	<!-- ====================================== -->
-
 </div><!-- #secondary -->
