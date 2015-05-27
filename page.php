f784eb03f4b88b860faa1649ea0b0216b7a8821b
Sunny Lam <sunny.lam@gmail.com>
2015-05-27 16:08:23 -0400

    Fixed pages.php

--- page.php
+++ page.php
@@ -17,8 +17,12 @@ get_header(); ?>
 	<p>This is the page.php</p>
 	<main id="main" class="site-main" role="main">
 
+		<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
 
 	</main><!-- #main -->
 </div><!-- #primary -->
