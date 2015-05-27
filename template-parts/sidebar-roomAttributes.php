fa624b266a802a7d69c147f6e98df54888d6a3c1
Sunny Lam <sunny.lam@gmail.com>
2015-05-27 17:25:44 -0400

    Changed sidebar-roomAttributes.php

--- template-parts/sidebar-roomAttributes.php
+++ template-parts/sidebar-roomAttributes.php
@@ -13,13 +13,15 @@
 
 	<h2>Room Attributes</h2>
 
+	<!-- on hover the span image changes colour, suggest trying colour change on the `li` -->
+	<!-- import Font-Awesome -->
 	<ul class="roomAttributes">
-		<li>Free WiFi Connection</li>
-		<li>Free Entry to the Rooftop Spa &amp; Health Club</li>
-		<li>Mineral water in the room upon arrival</li>
-		<li>24-hour room service</li>
-		<li>Multimedia TV, Minibar, In-room Safe, Individually Controlled A/C</li>
-		<li>24-Hour In Room Dining Service</li>
+		<li><div>Free WiFi Connection</div><div><span class="fa fa-wifi"></span></div></li>
+		<li><div>Free Entry to the Rooftop Spa &amp; Health Club</div><div><span class="fa fa-leaf"></span></div></li>
+		<li><div>Mineral water in the room upon arrival</div><div><span class="fa fa-tint"></span></div></li>
+		<li><div>24-hour room service</div><div><span class="fa fa-bell"></span></div></li>
+		<li><div>Multimedia TV, Minibar, In-room Safe, Individually Controlled A/C</div><div><span class="fa fa-star"></span></div></li>
+		<li><div>24-Hour In Room Dining Service</div><div><span class="fa fa-user"></span></div></li>
 	</ul>
 
 	<!-- ====================================== -->
