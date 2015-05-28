b748ec509e8a5cc9419e4cd66cfc3162f208bf86
Sunny Lam <sunny.lam@gmail.com>
2015-05-27 22:17:23 -0400

    Styling of room attributes widget of custom sidebar done.

--- template-parts/sidebar-roomAttributes.php
+++ template-parts/sidebar-roomAttributes.php
@@ -16,12 +16,12 @@
 	<!-- on hover the span image changes colour, suggest trying colour change on the `li` -->
 	<!-- import Font-Awesome -->
 	<ul class="roomAttributes">
-		<li><div>Free WiFi Connection</div><div><span class="fa fa-wifi"></span></div></li>
-		<li><div>Free Entry to the Rooftop Spa &amp; Health Club</div><div><span class="fa fa-leaf"></span></div></li>
-		<li><div>Mineral water in the room upon arrival</div><div><span class="fa fa-tint"></span></div></li>
-		<li><div>24-hour room service</div><div><span class="fa fa-bell"></span></div></li>
-		<li><div>Multimedia TV, Minibar, In-room Safe, Individually Controlled A/C</div><div><span class="fa fa-star"></span></div></li>
-		<li><div>24-Hour In Room Dining Service</div><div><span class="fa fa-user"></span></div></li>
+		<li><div><p>Free WiFi Connection</p></div><div><span class="fa fa-wifi"></span></div></li>
+		<li><div><p>Free Entry to the Rooftop Spa &amp; Health Club</p></div><div><span class="fa fa-leaf"></span></div></li>
+		<li><div><p>Mineral water in the room upon arrival</p></div><div><span class="fa fa-tint"></span></div></li>
+		<li><div><p>24-hour room service</p></div><div><span class="fa fa-bell"></span></div></li>
+		<li><div><p>Multimedia TV, Minibar, In-room Safe, Individually Controlled A/C</p></div><div><span class="fa fa-star"></span></div></li>
+		<li><div><p>24-Hour In Room Dining Service</p></div><div><span class="fa fa-user"></span></div></li>
 	</ul>
 
 	<!-- ====================================== -->
