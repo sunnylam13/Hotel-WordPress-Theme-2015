99f7f3bd82424ce0040b81f37731578c98552361
Sunny Lam <sunny.lam@gmail.com>
2015-05-28 12:57:01 -0400

    Styling the news page.

--- single.php
+++ single.php
@@ -8,6 +8,9 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
+
+		<!-- <p>This is single.php</p> -->
+
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
