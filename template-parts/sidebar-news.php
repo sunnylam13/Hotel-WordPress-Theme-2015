ae32cd9bdbf19849d91770cf2a2a1b6fcfcd9b53
Sunny Lam <sunny.lam@gmail.com>
2015-05-28 14:28:10 -0400

    Styling for news page mostly done.

---
+++ template-parts/sidebar-news.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template used for displaying the sidebar for page-suites.php
+ *
+ * @package hospitality
+ */
+?>
+
+<div id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+	<?php get_template_part( 'template-parts/sidebar', 'reservations' ); ?>
+
+</div><!-- #secondary -->
