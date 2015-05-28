5596ebc2bc53d95a524dd0b72117dbac63c9fe00
Dallas Koncir <me@dallaskoncir.com>
2015-05-28 14:25:26 -0400

    set up datepicker and selectric

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 // Doc ready function
 $(function() {
   // Load your custom js in here
+
+  // Load pickadate
+  $('.datepicker').pickadate();
+
+  // Load selectric
+  $('select').selectric();
 });
