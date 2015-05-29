7423ff6ca20ad2b1ecae58a25cafbe1e2a9ee9b7
Sunny Lam <sunny.lam@gmail.com>
2015-05-28 21:04:39 -0400

    Styled the single.php "News" post page (when you click on a blog post link).  Also added mixins-project.scss to quickly access common styling for for content links <a> and buttons.

--- single.php
+++ single.php
@@ -7,29 +7,34 @@
 
 get_header(); ?>
 
+<div class="flexbox container">
+
 	<div id="primary" class="content-area">
-
+	
 		<!-- <p>This is single.php</p> -->
-
+	
 		<main id="main" class="site-main" role="main">
-
+	
 		<?php while ( have_posts() ) : the_post(); ?>
-
+	
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
+	
 			<?php the_post_navigation(); ?>
-
+	
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
-
+	
 		<?php endwhile; // end of the loop. ?>
-
+	
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	
+	<?php get_template_part( 'template-parts/sidebar', 'news' ); ?>
+	
+</div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
